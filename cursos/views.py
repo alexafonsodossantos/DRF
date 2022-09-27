88b008e4e8d6c2f9189f2080d6225c4f32a326f2
alex <aa.santos95@gmail.com>
2022-09-27 17:17:35 -0300

    APIView override default

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+from rest_framework.generics import get_object_or_404
 
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
@@ -14,6 +15,20 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso=self.kwargs.get('curso_pk'))
+        else:
+            return self.queryset.all()
+
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(   self.get_queryset(),
+                                        curso_id = self.kwargs.get('curso_pk'),
+                                        pk=self.kwargs.get('avaliacao_pk'))
+        else:
+            return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
