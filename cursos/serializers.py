7e2a63306a53191593f66eee7f1209323afa55d7
alex <aa.santos95@gmail.com>
2022-09-29 16:10:18 -0300

    added rating average

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.db.models import Avg
 from .models import Curso, Avaliacao
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
@@ -19,6 +19,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'publicacao',
             'ativo'
         )
+    def validate_avaliacao(self, valor):
+        if valor in range(1,6):
+            return valor
+        else:
+            raise serializers.ValidationError("Erro: Nota precisa ser inteiro entre 1 e 5.")
 
 class CursoSerializer(serializers.ModelSerializer):
     
@@ -33,6 +38,7 @@ class CursoSerializer(serializers.ModelSerializer):
         )"""
 
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    media_avaliacoes = serializers.SerializerMethodField()
 
     class Meta:
         model = Curso
@@ -42,5 +48,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'publicacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+    
+    def get_media_avaliacoes(self,obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+        return round(media * 2 ) / 2
+        
